b303c9bbb4fe4baaa0333d9a61a7fa6d68950837
shuzhouliu <shuzhou.liu@hotmail.com>
2022-12-12 21:11:05 +0800

    feat: add ocr skeleton, text full

--- src/js/lang.js
+++ src/js/lang.js
@@ -8,14 +8,38 @@ let zh_CN = {
     "all_set": "<b>一切就绪！</b> 您现在可以使用Assistant帮助您背诵了。",
     "go_ahead": "继续",
     "add_piece": "+ 新增背诵",
-    "open_piece": "加载历史背诵"
+    "open_piece": "加载历史背诵",
+    "lr_type": "录入方式：",
+    "please_select": "请选择……",
+    "text": "文本",
+    "image": "图片",
+    "voice": "语音",
+    "audio": "音频",
+    "filename": "文件名称：",
+    "exist_alert": "这个文件已经存在，请您更换一个文件名。",
+    "name": "名称：",
+    "enter_prompt": "请输入背诵内容：",
+    "ocr_prompt": "请在<b>Assistant所在磁盘</b>中用一个文件夹单独放置您的图片，然后在下面输入文件夹路径，<b>不要输入盘符</b>。",
+    "image_prompt": "请您输入您的图片文件夹，<b>不要输入盘符</b>：",
 };
 
 let en_US = {
     "all_set": "<b>You're all set! </b> You can now use Assistant to help you with reciting.",
     "go_ahead": "Go ahead",
     "add_piece": "+ Add piece",
-    "open_piece": "Load history pieces"
+    "open_piece": "Load history pieces",
+    "lr_type": "Input type:",
+    "please_select": "Please select...",
+    "text": "Text",
+    "image": "Image",
+    "voice": "Voice",
+    "audio": "Audio",
+    "filename": "File name:",
+    "exist_alert": "This file already exists, please change the file name.",
+    "name": "Name:",
+    "enter_prompt": "Please enter the reciting content:",
+    "ocr_prompt": "Please put your images in a folder on <b>the disk where Assistant is located</b>, then enter the folder path below, <b>without the disk letter</b>.",
+    "image_prompt": "Please enter your image folder, <b>without the disk letter</b>:",
 };
 
 /**
@@ -33,4 +57,7 @@ function update(loc) {
     let sel = document.getElementsByTagName("loc");
     for (var i in sel)
         sel[i].innerHTML = lang[loc][sel[i].innerHTML];
+    let sel2 = document.getElementsByClassName("loc");
+    for (var i in sel2)
+        sel2[i].innerHTML = lang[loc][sel2[i].innerHTML];
 }
---
+++ src/js/ocr_general.js
@@ -0,0 +1,70 @@
+const CryptoJS = require('crypto-js')
+var request = require('request')
+var log = require('log4node')
+var fs = require('fs')
+
+function ocr(file) {
+
+    let json = JSON.parse(fc(config_folder() + "/.xfyun"));
+
+    const config = {
+        hostUrl: "https://webapi.xfyun.cn/v1/service/v1/ocr/general",
+        host: "webapi.xfyun.cn",
+        appid: json.appid,
+        apiKey: json.ise_apikey,
+        uri: "/v1/ise",
+        file: file
+    }
+
+    // 获取当前时间戳
+    let ts = parseInt(new Date().getTime() / 1000)
+
+    let options = {
+        url: config.hostUrl,
+        headers: getReqHeader(),
+        form: getPostBody()
+    }
+    // 返回结果json串
+    request.post(options, (err, resp, body) => {
+        if (err) {
+            log.error(err)
+        }
+        let res = JSON.parse(body)
+        if (res.code != 0) {
+            log.error(`发生错误，错误码：${res.code} 错误原因：${res.desc} sid：${res.sid}`)
+            log.error(`请前往https://www.xfyun.cn/document/error-code?code=${res.code}查询解决办法`)
+            return false
+        }
+        let resq = res.data.block;
+    })
+
+}
+
+// 组装业务参数
+function getXParamStr() {
+    let xParam = {
+        language: 'cn|en'
+    }
+    return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(JSON.stringify(xParam)))
+}
+
+// 组装请求头
+function getReqHeader() {
+    let xParamStr = getXParamStr()
+    let xCheckSum = CryptoJS.MD5(config.apiKey + ts + xParamStr).toString()
+    return {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-Appid': config.appid,
+        'X-CurTime': ts + "",
+        'X-Param': xParamStr,
+        'X-CheckSum': xCheckSum
+    }
+}
+
+// 组装postBody
+function getPostBody() {
+    let buffer = fs.readFileSync(config.file)
+    return {
+        image: buffer.toString('base64'),
+    }
+}
