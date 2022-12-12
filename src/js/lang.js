ba78dd79862f5f9d9c33b4caf08ce712eb2684fe
shuzhouliu <shuzhou.liu@hotmail.com>
2022-12-12 19:41:53 +0800

    feat: the locale system, and add_piece open_piece

--- src/js/lang.js
+++ src/js/lang.js
@@ -5,11 +5,17 @@
  */
 
 let zh_CN = {
-    "all_set": "<b>一切就绪！</b> 您现在可以使用Assistant帮助您背诵了。"
+    "all_set": "<b>一切就绪！</b> 您现在可以使用Assistant帮助您背诵了。",
+    "go_ahead": "继续",
+    "add_piece": "+ 新增背诵",
+    "open_piece": "加载历史背诵"
 };
 
 let en_US = {
-    "all_set": "<b>You're all set! </b> You can now use Assistant to help you with reciting."
+    "all_set": "<b>You're all set! </b> You can now use Assistant to help you with reciting.",
+    "go_ahead": "Go ahead",
+    "add_piece": "+ Add piece",
+    "open_piece": "Load history pieces"
 };
 
 /**
@@ -24,7 +30,7 @@ let lang = {"zh-CN": zh_CN, "en-US": en_US};
  * @param {string} loc 
  */
 function update(loc) {
-    let sel = document.querySelector("loc");
+    let sel = document.getElementsByTagName("loc");
     for (var i in sel)
-        sel[i].innerHTML = lang[loc][sel[i].label];
+        sel[i].innerHTML = lang[loc][sel[i].innerHTML];
 }
