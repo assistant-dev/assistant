3a9a76b4fd5953613b0b225f07a69dd93647efad
shuzhouliu <shuzhouliu@qq.com>
2022-12-17 13:44:04 +0800

    fix: theme errs

--- src/js/theming.js
+++ src/js/theming.js
@@ -29,29 +29,37 @@ function css_import(css) {
   document.head.appendChild(css_import);
 }
 
-function theme() {
+async function wc(url) {
+  const resp = await fetch(url);
+  return resp.text();
+}
+
+async function theme() {
   let cfg = JSON.parse(fc(config_path()));
-  let theme = cfg.theme;
-  let themehex = fc(`theme/${theme}.hex`);
+  let themez = cfg.theme;
+  let themehex = fc(`theme/${themez}.hex`);
   themehex = hextodec(themehex);
   themehex = dectochar(themehex);
   themehex = JSON.parse(themehex);
-  for (let i = 0; i < themehex["@include"].length; i++) {
-    css_import(themehex["@include"][i]);
+  if (typeof themehex["@include"] !== "undefined") {
+    for (let i = 0; i < themehex["@include"].length; i++) {
+      css_import(themehex["@include"][i]);
+    }
   }
-  for (let i = 0; i < themehex["@script-url"].length; i++) {
-    let script = document.createElement("script");
-    script.setAttribute("src", themehex["@script-url"][i]);
-    document.body.appendChild(script);
+  if (typeof themehex["@script"] !== "undefined") {
+    for (let i = 0; i < themehex["@script-url"].length; i++)
+      eval(await wc(themehex["@script-url"][i]));
   }
-  setTimeout(themehex["@additional-script"], 0);
-  let cls = document.getElementsByClassName("ast-theme");
-  for (let i = 0; i < cls.length; i++) {
-    let t = cls[i].dataset.theme;
-    cls.className = themehex[t];
+  if (typeof themehex["@script"] !== "undefined") {
+    setTimeout(themehex["@additional-script"], 0);
   }
+  const cls = document.getElementsByClassName("ast-theme");
+  let arr = [];
+  for (let i = 0; i < cls.length; i++) arr.push(cls[i]);
+  for (let i = 0; i < arr.length; i++)
+    arr[i].className = themehex[arr[i].dataset.theme];
 }
 
-(function () {
-  theme();
+(async function () {
+  await theme();
 })();
